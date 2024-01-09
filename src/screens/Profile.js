ce9a250d30a907613702bb3bf5f65b62d02301bd
molokochris <molokochrisp742@gmail.com>
2024-01-09 12:38:53 +0200

    ui adds

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button } from "react-native";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
-export default function Profile() {
+export default function Profile({ navigation }) {
+  const { logout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    logout();
+  };
   return (
-    <View>
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
       <Text>Profile</Text>
+      <Button title="logout" color={"tomato"} onPress={handleLogout} />
     </View>
   );
 }
